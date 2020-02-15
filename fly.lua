c0f5fab8874318df8347cf5ba8d956b9ca2ffa15
Helldragger <chritopher1223334444@gmail.com>
2020-02-15 17:09:28 +0100

    deparallelized functions to test things out

--- fly.lua
+++ fly.lua
@@ -28,21 +28,15 @@ local stop = false
 local meta = modules.getMetaOwner()
 
 local function refreshMeta()
-    while not stop do
-        if DEBUG then print("refresh meta") end
-        yield()
-        meta = modules.getMetaOwner()
-    end
+    if DEBUG then print("refresh meta") end
+    meta = modules.getMetaOwner()
 end
 
 -- LOCATION / HEIGHT ABOVE GROUND CACHE
 local scanned = modules.scan()
 local function refreshScan()
-    while not stop do
-        if DEBUG then print("refresh scan") end
-        yield()
-        scanned = modules.scan()
-    end
+    if DEBUG then print("refresh scan") end
+    scanned = modules.scan()
 end
 
 -- LOCATION HELPER
@@ -72,98 +66,95 @@ local hover = false
 local in_flight = false
 
 local function controls()
-    while not stop do 
-        if DEBUG then print("controls") end
-        yield()
-        local speed = (meta.motionX^2 + meta.motionY^2)^0.5
-        local event, key, held = os.pullEvent()
-        -- print( "[" ... event ... "] " ... key ) 
-        if event == "key" and key == keys.k then
-            stop = true
-            print("K pressed, stopping program...")
-            
-        elseif event == "key" and key == keys.i and not held then
-            print("[INFO] pitch " .. meta.pitch .. " | yaw " .. meta.yaw .. " | velocity " .. meta.motionX .. "x " .. meta.motionY .. "y |")
-        elseif event == "key" and key == keys.space and not held then    
-            spacePressed = true
-            local spaceTime = os.clock()
-            local diff = spaceTime - lastSpaceTime
-            if (diff < 0.5) then
-                fly = not fly
-                spaceTime = -1
-                if fly then 
-                    print("FLY MODE ENABLED")
-                else 
-                    print("FLY MODE DISABLED") 
-                end                    
-            end 
-            lastSpaceTime = spaceTime
-            -- the space key launches you in whatever direction you are looking at
-            --modules.launch(meta.yaw, meta.pitch, 0.6)
-        elseif event == "key" and key == keys.x and not held then
-            -- holding the X key enables "hover"mode, mwe disab when it is released.
-            if not hover then
-                hover = true
-                os.queueEvent("hover")
-            end
-        elseif event == "key_up" and key == keys.x then
-            hover = false
-        elseif event == "key_up" and key == keys.space then
-            spacePressed = false
+    if DEBUG then print("controls") end
+    local speed = (meta.motionX^2 + meta.motionY^2)^0.5
+    local event, key, held = os.pullEvent()
+    -- print( "[" ... event ... "] " ... key ) 
+    if event == "key" and key == keys.k then
+        stop = true
+        print("K pressed, stopping program...")
+        
+    elseif event == "key" and key == keys.i and not held then
+        print("[INFO] pitch " .. meta.pitch .. " | yaw " .. meta.yaw .. " | velocity " .. meta.motionX .. "x " .. meta.motionY .. "y |")
+    elseif event == "key" and key == keys.space and not held then    
+        spacePressed = true
+        local spaceTime = os.clock()
+        local diff = spaceTime - lastSpaceTime
+        if (diff < 0.5) then
+            fly = not fly
+            spaceTime = -1
+            if fly then 
+                print("FLY MODE ENABLED")
+            else 
+                print("FLY MODE DISABLED") 
+            end                    
+        end 
+        lastSpaceTime = spaceTime
+        -- the space key launches you in whatever direction you are looking at
+        --modules.launch(meta.yaw, meta.pitch, 0.6)
+    elseif event == "key" and key == keys.x and not held then
+        -- holding the X key enables "hover"mode, mwe disab when it is released.
+        if not hover then
+            hover = true
+            os.queueEvent("hover")
         end
-
-        -- FLIGHT RELATED
-        -- shift => descente
-        if key == keys.shift then
-            if event == "key" then 
-                down = true 
-            elseif event == "key_up" then
-                down = false
-            end
-        end
-        -- space => montée 
-        if key == keys.space then 
-            if event == "key" then
-                up = true 
-            elseif event == "key_up" then
-                up = false
-            end
-        end
-        -- W => en avant
-        if key == keys.w then
-            if event == "key" then
-                front = true
-            elseif event == "key_up" then
-                front = false
-            end
-        end
-        -- S => en arrière 
-        if key == keys.s then
-            if event == "key" then
-                back = true
-            elseif event == "key_up" then
-                back = false
-            end
-        end
-        -- A => à gauche
-        if key == keys.a then
-            if event == "key" then
-                left = true
-            elseif event == "key_up" then
-                left = false
-            end
-        end
-        -- D => à droite
-        if key == keys.d then
-            if event == "key" then
-                right = true
-            elseif event == "key_up" then
-                right = false
-            end
-        end
-        -- on check le block sous les pieds du joueur
-        in_flight = scannedAt(8,0,8).name ~= "minecraft:air"
+    elseif event == "key_up" and key == keys.x then
+        hover = false
+    elseif event == "key_up" and key == keys.space then
+        spacePressed = false
     end
+
+    -- FLIGHT RELATED
+    -- shift => descente
+    if key == keys.shift then
+        if event == "key" then 
+            down = true 
+        elseif event == "key_up" then
+            down = false
+        end
+    end
+    -- space => montée 
+    if key == keys.space then 
+        if event == "key" then
+            up = true 
+        elseif event == "key_up" then
+            up = false
+        end
+    end
+    -- W => en avant
+    if key == keys.w then
+        if event == "key" then
+            front = true
+        elseif event == "key_up" then
+            front = false
+        end
+    end
+    -- S => en arrière 
+    if key == keys.s then
+        if event == "key" then
+            back = true
+        elseif event == "key_up" then
+            back = false
+        end
+    end
+    -- A => à gauche
+    if key == keys.a then
+        if event == "key" then
+            left = true
+        elseif event == "key_up" then
+            left = false
+        end
+    end
+    -- D => à droite
+    if key == keys.d then
+        if event == "key" then
+            right = true
+        elseif event == "key_up" then
+            right = false
+        end
+    end
+    -- on check le block sous les pieds du joueur
+    in_flight = scannedAt(8,0,8).name ~= "minecraft:air"
 end
 
 
@@ -174,49 +165,40 @@ end
 -- i.e.: north = launch(0, 0, power)
 
 local function flyMode()
-    while not stop do
-        if DEBUG then print("fly") end
-        yield()
-        if fly then
-            -- si au sol => fly mode desactivé
-            if not in_flight and not up then
-                fly = false
-                print("Ground reached, fly disabled")
-            end
+    if DEBUG then print("fly") end
+    if fly then
+        -- si au sol => fly mode desactivé
+        if not in_flight and not up then
+            fly = false
+            print("Ground reached, fly disabled")
         end
     end
 end
 
 local function hoverMode()
-    while not stop do
-        if DEBUG then print("hover") end
-        yield()
-        if hover then
-            local mY = meta.motionY
-            mY = (mY - 0.138) / 0.8
-            if mY > 0.5 or mY < 0 then
-                local sign = 1
-                if mY < 0 then sign = -1 end
-                modules.launch(meta.yaw, 90 * sign, math.min(4, math.abs(mY)))
-            end
+    if DEBUG then print("hover") end
+    if hover then
+        local mY = meta.motionY
+        mY = (mY - 0.138) / 0.8
+        if mY > 0.5 or mY < 0 then
+            local sign = 1
+            if mY < 0 then sign = -1 end
+            modules.launch(meta.yaw, 90 * sign, math.min(4, math.abs(mY)))
         end
     end
 end
 
 
 local function fallCushion()
-    while not stop do
-        if DEBUG then print("fall cushion") end
-        yield()
-        if in_flight and meta.motionY < -0.3 then
-            for y = 0, -8, -1 do
-                local block = scannedAt(8,y,8)
-                if block.name ~= "minecraft:air" then
-                    modules.launch( meta.yaw,
-                                    -90, 
-                                    math.min(4, meta.motionY / -0.5))
-                    break
-                end
+    if DEBUG then print("fall cushion") end
+    if in_flight and meta.motionY < -0.3 then
+        for y = 0, -8, -1 do
+            local block = scannedAt(8,y,8)
+            if block.name ~= "minecraft:air" then
+                modules.launch( meta.yaw,
+                                -90, 
+                                math.min(4, meta.motionY / -0.5))
+                break
             end
         end
     end
@@ -225,11 +207,20 @@ end
 -- MAIN LOOP
 print("FLY program started, press K to stop")
 
-parallel.waitForAny(
-    refreshMeta,
-    refreshScan,
-    controls,
-    flyMode,
-    hoverMode,
-    fallCushion
-)
+while not stop do
+    yield()
+    refreshMeta()
+    refreshScan()
+    controls()
+    flyMode()
+    hoverMode()
+    fallCushion()
+end
+--parallel.waitForAny(
+--    refreshMeta,
+--    refreshScan,
+--    controls,
+--    flyMode,
+--    hoverMode,
+--    fallCushion
+--)
