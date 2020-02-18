c35aeac3105df258e3d71b6682d8c43848b7e5d7
Helldragger <chritopher1223334444@gmail.com>
2020-02-18 22:33:13 +0100

    add overlay stub

--- flyBalancer.lua
+++ flyBalancer.lua
@@ -25,10 +25,10 @@ local modules = peripheral.find("neuralInterface")
 if not modules then error("Must have a neural interface", 0) end
 
 -- MODULES REQUIRED
-if not modules.hasModule("plethora:sensor") then error("Must have a sensor", 0) end
-if not modules.hasModule("plethora:scanner") then error("Must have a scanner", 0) end
+if not modules.hasModule("plethora:sensor") then error("Must have a sensor", 0) end 
 if not modules.hasModule("plethora:introspection") then error("Must have an introspection module", 0) end
 if not modules.hasModule("plethora:kinetic", 0) then error("Must have a kinetic agument", 0) end
+if not modules.hasModule("plethora:glasses") then error("The overlay glasses are missing", 0) end
 
 -- DEBUG CONTROL
 local DEBUGCALLS = false
@@ -46,35 +46,21 @@ local function refreshMeta()
     meta = modules.getMetaOwner()
 end
 
--- LOCATION / HEIGHT ABOVE GROUND CACHE
-local scanned = modules.scan()
-local function refreshScan()    
-    os.pullEvent("refreshScan")
-    if DEBUGCALLS then printDebug("refresh scan") end
-    scanned = modules.scan()
-end
-
--- LOCATION HELPER
-local scanner_radius = 8
-local scanner_width = scanner_radius*2 + 1
-
-local function scannedAt(x,y,z)
-    return scanned[scanner_width ^ 2 * (x + scanner_radius) + scanner_width * (y + scanner_radius) + (z + scanner_radius) + 1]
-end
-
 -- CONTROLS
-local LIGHTSPEED = 4
-local FASTER = 2
-local FAST = 1
-local NORMAL = 0.5
+local LIGHTSPEED = 1
+local FASTER = 0.5
+local FAST = 0.3
+local NORMAL = 0.2
 local SPEEDMODE = NORMAL
 
-local MAX_THRUST = 0.2
+local MAX_THRUST = SPEEDMODE
 local MIN_THRUST = 0.15
+local THRUST_GRADIENT = (MAX_THRUST - MIN_THRUST) / 20
 local ACTUAL_THRUST = (MAX_THRUST - MIN_THRUST) / 2
 
 local MAX_PITCH = 90
 local MIN_PITCH = -90
+local PITCH_GRADIENT = (MAX_PITCH - MIN_PITCH) / 20 
 local ACTUAL_PITCH = (MAX_PITCH - MIN_PITCH) / 2
 
 
@@ -98,10 +84,6 @@ local left = false
 
 local lastSpaceTime = -1
 local spacePressed = false
-
-local hover = false
-
-local in_flight = false
 
 local FLYCALLSSINCELASTCONTROL = 1
 
@@ -149,25 +131,25 @@ local function controls()
     if key == keys.period  then
         if SPEEDMODE == NORMAL then 
             SPEEDMODE = FAST
-            print("Speed mode set to FAST")
+            print("Speed mode set to FAST (warning: high altitude might lead to death by asphyxia)")
         elseif SPEEDMODE == FAST then 
             SPEEDMODE = FASTER
-            print("Speed mode set to FASTER")
+            print("Speed mode set to FASTER (WARNING: can reach deadly altitude VERY quick!)")
         elseif SPEEDMODE == FASTER then 
             SPEEDMODE = LIGHTSPEED
-            print("Speed mode set to LIGHTSPEED")
+            print("Speed mode set to LIGHTSPEED (BIG WARNING: might reach deadly altitude in LESS than a few second!)")
         else
-            print("Speed mode is already maximal")
+            print("Speed mode is already maximal (warning: high altitude might lead to death by asphyxia)")
         end
     end
     -- comma (,) => slowdown
     if key == keys.comma then
         if SPEEDMODE == LIGHTSPEED then 
             SPEEDMODE = FASTER
-            print("Speed mode set to FASTER")
+            print("Speed mode set to FASTER (WARNING: can reach deadly altitude VERY quick!)")
         elseif SPEEDMODE == FASTER then 
             SPEEDMODE = FAST
-            print("Speed mode set to FAST")
+            print("Speed mode set to FAST (warning: high altitude might lead to death by asphyxia)")
         elseif SPEEDMODE == FAST then 
             SPEEDMODE = NORMAL
             print("Speed mode set to NORMAL")
@@ -205,8 +187,6 @@ local function controls()
         right = true
         rightLastPressedTime = os.clock()
     end
-    -- on check le block sous les pieds du joueur
-    in_flight = scannedAt(8,-1,8).name ~= "minecraft:air"
     if DEBUGINPUT then
         local pressed = ""
         if up then pressed = pressed.."UP " end
@@ -219,40 +199,6 @@ local function controls()
     end
     -- on refresh nos données
     os.queueEvent("refreshMeta")
-    os.queueEvent("refreshScan")
-end
-
-
--- pitch = vertical
--- yaw = horizontal
--- both use -180 -> 180 degrees
--- lauynche(yaw, pitch, power)
--- i.e.: up = launch(0, -90, power)
--- north: -180|180
--- south: 0|360
--- east : -90|280
--- west : -280|90
---
---  0    --> 360
---  -360 --> 0
---
--- 2.W   3.N
---   \   /
---     X
---   /   \
--- 1.S   4.E     
--- Sens horaire so to the right = theta > 0
--- to the left theta < 0
---
-
-local function addYaw(theta, delta)
-    theta = theta + delta
-    if theta < -360 then
-        theta = theta + 360
-    elseif theta > 360 then
-        theta = theta - 360
-    end
-    return theta
 end
 
 local function flyMode()
@@ -261,37 +207,27 @@ local function flyMode()
     if DEBUGCALLS then printDebug("fly") end
     if fly then
         FLYCALLSSINCELASTCONTROL = FLYCALLSSINCELASTCONTROL + 1
-            -- si au sol => fly mode desactivé
-        
-        --if not in_flight and not up and (os.clock()-flyActivatedTime) > 0.5 then
-        --    fly = false
-        --    print("Ground reached, fly disabled")
-        --    return
-        --end
-
-        -- YAW (horizontal)
         
         if back then 
-
-            ACTUAL_PITCH = ACTUAL_PITCH + 5
+            ACTUAL_PITCH = ACTUAL_PITCH + PITCH_GRADIENT
             if ACTUAL_PITCH > MAX_PITCH then ACTUAL_PITCH = MAX_PITCH end
             back = false
         end
 
         if front then 
-            ACTUAL_PITCH = ACTUAL_PITCH - 5
+            ACTUAL_PITCH = ACTUAL_PITCH - PITCH_GRADIENT
             if ACTUAL_PITCH < MIN_PITCH then ACTUAL_PITCH = MIN_PITCH end
             front = false
         end
 
         if right then 
-            ACTUAL_THRUST = ACTUAL_THRUST + 0.01
+            ACTUAL_THRUST = ACTUAL_THRUST + THRUST_GRADIENT
             if ACTUAL_THRUST > MAX_THRUST then ACTUAL_THRUST = MAX_THRUST end
             right = false
         end 
 
         if left then 
-            ACTUAL_THRUST = ACTUAL_THRUST - 0.01
+            ACTUAL_THRUST = ACTUAL_THRUST - THRUST_GRADIENT
             if ACTUAL_THRUST < MIN_THRUST then ACTUAL_THRUST = MIN_THRUST end
             left = false
         end 
@@ -303,6 +239,16 @@ local function flyMode()
         os.queueEvent("fly")
     end
 end
+
+local function overlay()
+    -- Get hold of the canvas
+    local interface = peripheral.wrap("back")
+    local canvas = interface.canvas()
+    -- And add a rectangle
+    local rect = canvas.addRectangle(0, 0, 100, 100, 0xFF0000FF)
+
+end
+
 
 local function untilKill(func, doesYield)
     while not stop do
@@ -319,18 +265,12 @@ parallel.waitForAny(
         untilKill(refreshMeta, false)
     end,
     function() 
-        untilKill(refreshScan, false)
-    end,
-    function() 
         untilKill(controls, false)
     end,
     function() 
+        untilKill(overlay, false)
+    end,
+    function() 
         untilKill(flyMode, false)
-    end--,
-    --function() 
-    --    untilKill(hoverMode)
-    --end,
-    --function() 
-    --    untilKill(fallCushion)
-    --end
+    end
 )
