a9ef80fb93a26846a8ffd41ddcfb3d5ad7af23c4
Helldragger <chritopher1223334444@gmail.com>
2020-02-19 17:29:40 +0100

    replace old fly MK I with fly MK III

--- fly.lua
+++ fly.lua
@@ -13,11 +13,11 @@ local DEBUG_LOG_FILE = "./fly_debug.log"
 if fs.exists(DEBUG_LOG_FILE) then fs.delete(DEBUG_LOG_FILE) end
 
 local function printDebug(msg)
-    msg = "["..os.date().."] "..msg.."\n"
-    --print(msg)
-    local log = fs.open(DEBUG_LOG_FILE, "a")
-    log.write(msg)
-    log.close()
+    msg = msg.."\n"
+    print(msg)
+    --local log = fs.open(DEBUG_LOG_FILE, "a")
+    --log.write(msg)
+    --log.close()
 end
 
 -- NEURAL INTERFACE REQUIRED
@@ -25,14 +25,14 @@ local modules = peripheral.find("neuralInterface")
 if not modules then error("Must have a neural interface", 0) end
 
 -- MODULES REQUIRED
-if not modules.hasModule("plethora:sensor") then error("Must have a sensor", 0) end
-if not modules.hasModule("plethora:scanner") then error("Must have a scanner", 0) end
+if not modules.hasModule("plethora:sensor") then error("Must have a sensor", 0) end 
 if not modules.hasModule("plethora:introspection") then error("Must have an introspection module", 0) end
 if not modules.hasModule("plethora:kinetic", 0) then error("Must have a kinetic agument", 0) end
+if not modules.hasModule("plethora:glasses") then error("The overlay glasses are missing", 0) end
 
 -- DEBUG CONTROL
 local DEBUGCALLS = false
-local DEBUGINPUT = false
+local DEBUGINPUT = true
 
 -- KILL SWITCH CONTROL
 local stop = false
@@ -46,28 +46,23 @@ local function refreshMeta()
     meta = modules.getMetaOwner()
 end
 
--- LOCATION / HEIGHT ABOVE GROUND CACHE
-local scanned = modules.scan()
-local function refreshScan()    
-    os.pullEvent("refreshScan")
-    if DEBUGCALLS then printDebug("refresh scan") end
-    scanned = modules.scan()
-end
-
--- LOCATION HELPER
-local scanner_radius = 8
-local scanner_width = scanner_radius*2 + 1
-
-local function scannedAt(x,y,z)
-    return scanned[scanner_width ^ 2 * (x + scanner_radius) + scanner_width * (y + scanner_radius) + (z + scanner_radius) + 1]
-end
-
 -- CONTROLS
 local LIGHTSPEED = 4
-local FASTER = 2
+local FASTER = 2.5
 local FAST = 1
-local NORMAL = 0.5
+local NORMAL = 0.2
 local SPEEDMODE = NORMAL
+
+local MAX_THRUST = SPEEDMODE
+local MIN_THRUST = 0.15
+local THRUST_GRADIENT = (MAX_THRUST - MIN_THRUST) / 10
+local ACTUAL_THRUST = ((MAX_THRUST - MIN_THRUST) / 2)+MIN_THRUST
+
+local MAX_PITCH = 90
+local MIN_PITCH = -90
+local PITCH_GRADIENT = 45/5 --(MAX_PITCH - MIN_PITCH) / 10 
+local ACTUAL_PITCH = 90 --((MAX_PITCH - MIN_PITCH) / 2)+MIN_PITCH
+
 
 local fly = false
 local flyActivatedTime = -1
@@ -90,11 +85,16 @@ local left = false
 local lastSpaceTime = -1
 local spacePressed = false
 
-local hover = false
-
-local in_flight = false
-
 local FLYCALLSSINCELASTCONTROL = 1
+
+local function addPitch(theta, delta)
+    theta = math.fmod( theta + delta, 360 )
+    if theta < 0 then
+        theta = theta + 360
+    end
+    return theta
+end
+
 
 local function controls()
     local event, key, held = os.pullEvent("key")
@@ -140,64 +140,76 @@ local function controls()
     if key == keys.period  then
         if SPEEDMODE == NORMAL then 
             SPEEDMODE = FAST
-            print("Speed mode set to FAST")
+            print("Speed mode set to FAST (warning: high altitude might lead to death by asphyxia)")
         elseif SPEEDMODE == FAST then 
             SPEEDMODE = FASTER
-            print("Speed mode set to FASTER")
+            print("Speed mode set to FASTER (WARNING: can reach deadly altitude VERY quick!)")
         elseif SPEEDMODE == FASTER then 
             SPEEDMODE = LIGHTSPEED
-            print("Speed mode set to LIGHTSPEED")
+            print("Speed mode set to LIGHTSPEED (BIG WARNING: might reach deadly altitude in LESS than a few second!)")
         else
-            print("Speed mode is already maximal")
+            print("Speed mode is already maximal (warning: high altitude might lead to death by asphyxia)")
         end
+        MAX_THRUST = SPEEDMODE
+        THRUST_GRADIENT = (MAX_THRUST - MIN_THRUST) / 10
     end
     -- comma (,) => slowdown
     if key == keys.comma then
         if SPEEDMODE == LIGHTSPEED then 
             SPEEDMODE = FASTER
-            print("Speed mode set to FASTER")
+            print("Speed mode set to FASTER (WARNING: can reach deadly altitude VERY quick!)")
         elseif SPEEDMODE == FASTER then 
             SPEEDMODE = FAST
-            print("Speed mode set to FAST")
+            print("Speed mode set to FAST (warning: high altitude might lead to death by asphyxia)")
         elseif SPEEDMODE == FAST then 
             SPEEDMODE = NORMAL
             print("Speed mode set to NORMAL")
         else
             print("Speed mode is already minimal")
         end
+        MAX_THRUST = SPEEDMODE
+        THRUST_GRADIENT = (MAX_THRUST - MIN_THRUST) / 10
     end
     -- shift => descente
-    if key == keys.shift then
+    if key == keys.leftShift then
         down = true
         downLastPressedTime = os.clock()
+        ACTUAL_THRUST = ACTUAL_THRUST - THRUST_GRADIENT
+        if ACTUAL_THRUST < MIN_THRUST then ACTUAL_THRUST = MIN_THRUST end
+
     end
     -- space => montée 
     if key == keys.space then 
         up = true
         upLastPressedTime = os.clock()
+        ACTUAL_THRUST = ACTUAL_THRUST + THRUST_GRADIENT
+        if ACTUAL_THRUST > MAX_THRUST then ACTUAL_THRUST = MAX_THRUST end
+
     end
     -- W => en avant
-    if key == keys.w then
+    if key == keys.up then
         front = true
         frontLastPressedTime = os.clock()
+        ACTUAL_PITCH = addPitch(ACTUAL_PITCH, PITCH_GRADIENT)
+
     end
     -- S => en arrière 
-    if key == keys.s then
+    if key == keys.down then
         back = true
         backLastPressedTime = os.clock()
+        ACTUAL_PITCH = addPitch(ACTUAL_PITCH, -PITCH_GRADIENT)
+
     end
     -- A => à gauche
-    if key == keys.a then
+    if key == keys.left then
         left = true
         leftLastPressedTime = os.clock()
     end
     -- D => à droite
-    if key == keys.d then
+    if key == keys.right then
         right = true
         rightLastPressedTime = os.clock()
     end
-    -- on check le block sous les pieds du joueur
-    in_flight = scannedAt(8,-1,8).name ~= "minecraft:air"
     if DEBUGINPUT then
         local pressed = ""
         if up then pressed = pressed.."UP " end
@@ -210,187 +222,78 @@ local function controls()
     end
     -- on refresh nos données
     os.queueEvent("refreshMeta")
-    os.queueEvent("refreshScan")
 end
 
-
--- pitch = vertical
--- yaw = horizontal
--- both use -180 -> 180 degrees
--- lauynche(yaw, pitch, power)
--- i.e.: up = launch(0, -90, power)
--- north: -180|180
--- south: 0|360
--- east : -90|280
--- west : -280|90
---
---  0    --> 360
---  -360 --> 0
---
--- 2.W   3.N
---   \   /
---     X
---   /   \
--- 1.S   4.E     
--- Sens horaire so to the right = theta > 0
--- to the left theta < 0
---
-
-local function addYaw(theta, delta)
-    theta = theta + delta
-    if theta < -360 then
-        theta = theta + 360
-    elseif theta > 360 then
-        theta = theta - 360
-    end
-    return theta
-end
 
 local function flyMode()
     os.pullEvent("fly")
     
-    if DEBUGCALLS then printDebug("fly") end
+    if DEBUGCALLS then printDebug("fly") end    
+    -- APPLY        
     if fly then
-        FLYCALLSSINCELASTCONTROL = FLYCALLSSINCELASTCONTROL + 1
-            -- si au sol => fly mode desactivé
-        
-        --if not in_flight and not up and (os.clock()-flyActivatedTime) > 0.5 then
-        --    fly = false
-        --    print("Ground reached, fly disabled")
-        --    return
-        --end
-
-        -- YAW (horizontal)
-        if DEBUGINPUT then printDebug("fly: YAW CALCULATION") end
-        local dX = 0
-        local dZ = 0
-
-        
-        if left then 
-            if DEBUGINPUT then printDebug("fly: LEFT INFLUENCE") end
-            dX = dX - 1
-        end
-
-        if right then 
-            if DEBUGINPUT then printDebug("fly: RIGHT INFLUENCE") end
-            dX = dX + 1
-        end
-
-        if front then 
-            if DEBUGINPUT then printDebug("fly: FRONT INFLUENCE") end
-            dZ = dZ + 1
-        end 
-
-        if back then 
-            if DEBUGINPUT then printDebug("fly: BACK INFLUENCE") end
-            dZ = dZ - 1
-        end 
-
-        local dYaw = math.atan2(dX, dZ)
-
-        if DEBUGINPUT then printDebug("fly: ITERATIONS SINCE LAST CONTROL INFLUENCE") end
-        dYaw = dYaw / FLYCALLSSINCELASTCONTROL
-        if DEBUGINPUT then printDebug("fly: dYaw = "..dYaw) end
-        
-        if DEBUGINPUT then printDebug("fly: APPLY DELTA TO YAW") end
-        local yaw  = addYaw(meta.yaw, dYaw)        
-        if DEBUGINPUT then printDebug("fly: yaw = "..meta.yaw.." + "..dYaw.." = "..yaw) end
-        
-        if DEBUGINPUT then printDebug("fly: yaw = "..meta.yaw.." + "..dYaw.." = "..yaw) end
-
-        -- PITCH (vertical)
-        if DEBUGINPUT then printDebug("fly: PITCH CALCULATION") end
-        local dY = 0 -- 1.3 block/s gravity calculated, we aim to stabilize the player
-        
-        if up then 
-            if DEBUGINPUT then printDebug("fly: UP INFLUENCE") end
-            dY = dY - 90
-        end           
-
-        if down then 
-            if DEBUGINPUT then printDebug("fly: DOWN INFLUENCE") end
-            dY = dY + 90
-        end           
-        
-        local dPitch = dY
-        
-        if DEBUGINPUT then printDebug("fly: ITERATIONS SINCE LAST CONTROL INFLUENCE") end
-        dPitch = dPitch / FLYCALLSSINCELASTCONTROL
-        if DEBUGINPUT then printDebug("fly: Delta = "..dPitch) end
-
-        if DEBUGINPUT then printDebug("fly: APPLY DELTA TO PITCH") end
-        local pitch = dPitch 
-        --meta.pitch + dPitch       
-        if DEBUGINPUT then printDebug("fly: pitch = "..meta.pitch.." + "..dPitch.." = "..pitch) end
-
-
-        -- POWER (speed)
-        if DEBUGINPUT then printDebug("fly: POWER CALCULATION") end
-        local delta = 0
-        if DEBUGINPUT then printDebug("fly: Delta = "..delta) end
-        
-        if left or right or front or back then 
-            if DEBUGINPUT then printDebug("fly: HORIZONTAL INFLUENCE") end
-            delta = delta+0.01
-            if DEBUGINPUT then printDebug("fly: Delta = "..delta) end
-        end
-
-        if up or down then 
-            if DEBUGINPUT then printDebug("fly: VERTICAL INFLUENCE") end
-            delta = delta+0.04
-            if DEBUGINPUT then printDebug("fly: Delta = "..delta) end
-        end
-        
-        if DEBUGINPUT then printDebug("fly: ITERATIONS SINCE LAST CONTROL INFLUENCE") end
-        delta = delta / FLYCALLSSINCELASTCONTROL
-        if DEBUGINPUT then printDebug("fly: Delta = "..delta) end
-
-        if DEBUGINPUT then printDebug("fly: DIMINISHING RETURNS INFLUENCE") end
-        local speed = (meta.motionY^2 + (meta.motionX^2 + meta.motionZ^2)/10)^0.5 / FLYCALLSSINCELASTCONTROL
-        local MAXSPEED = SPEEDMODE -- max is 4
-        local power = math.min(MAXSPEED, speed+delta)
-        if DEBUGINPUT then printDebug("fly: current speed = "..speed) end
-        if DEBUGINPUT then printDebug("fly: max speed = "..MAXSPEED) end
-        if DEBUGINPUT then printDebug("fly: power = min("..MAXSPEED..","..speed+delta..") = "..power) end
-        
-        -- APPLY        
-        if DEBUGINPUT then printDebug("fly: APPLY FLY VECTOR") end
-        if DEBUGINPUT then printDebug("fly: launch("..yaw..", "..pitch..", "..power..")") end
-        modules.launch(yaw, pitch, power)
+        FLYCALLSSINCELASTCONTROL = FLYCALLSSINCELASTCONTROL + 1    
+        if DEBUGINPUT then printDebug("fly: launch(\n\tyaw: "..meta.yaw..",\n\tpitch: "..ACTUAL_PITCH..",\n\tthrust: "..ACTUAL_THRUST..")") end
+        -- we shift the pitch in order to get up at 90 degrees and 0 at horizontal.
+        modules.launch(meta.yaw,math.fmod( -ACTUAL_PITCH , 360), ACTUAL_THRUST)
         os.queueEvent("fly")
     end
 end
 
-local function hoverMode()
-    os.pullEvent("hover")
-    if DEBUGCALLS then printDebug("hover") end
-    if hover then
-        local mY = meta.motionY
-        mY = (mY - 0.138) / 0.8
-        if mY > 0.5 or mY < 0 then
-            local sign = 1
-            if mY < 0 then sign = -1 end
-            modules.launch(meta.yaw, 90 * sign, math.min(4, math.abs(mY)))
+local function getOrientation(pitch)
+
+    --                 ^ 90
+    --                 |
+    --                 |
+    -- 180 *-----------|------------> 0
+    --                 |
+    --                 |
+    --                 * 270
+
+
+    if  (pitch >= 0) then
+        if (pitch < 45 or pitch >= 315) then
+            return "front"
+        elseif (pitch < 135 )  then
+            return "up"
+        elseif (pitch < 225 ) then
+            return "back"
+        else 
+            return "down"
         end
     end
 end
 
 
-local function fallCushion()    
-    os.pullEvent("fallCushion")
-    if DEBUGCALLS then printDebug("fall cushion") end
-    if in_flight and not down and not up and meta.motionY < -0.3 then
-        for y = 0, -8, -1 do
-            local block = scannedAt(8,y,8)
-            if block.name ~= "minecraft:air" then
-                modules.launch( meta.yaw,
-                                -90, 
-                                math.min(4, meta.motionY / -0.5))
-                break
-            end
-        end
-    end
+-- Get hold of the canvas
+local interface = peripheral.wrap("back")
+local canvas = interface.canvas()
+canvas.clear()
+-- And add a rectangle
+
+local function round(value)
+    return math.floor(value * 100)/100
 end
+local speedgroup = canvas.addGroup({10,0})
+speedgroup.addText({10,10}, "Vertical")
+local YSpeed =speedgroup.addText({10,20}, round(meta.motionY).."b/s")
+speedgroup.addText({10,30}, "South-North")
+local ZSpeed =speedgroup.addText({10,40}, round(meta.motionZ).."b/s")
+speedgroup.addText({10,50}, "West-East")
+local XSpeed = speedgroup.addText({10,60}, round(meta.motionX).."b/s")
+speedgroup.addText({10,70}, "Thrust")
+local ThrustSpeed = speedgroup.addText({10,80}, round(ACTUAL_THRUST).."%")
+speedgroup.addText({10,90}, "Pitch")
+local PitchSpeed = speedgroup.addText({10,100}, round(ACTUAL_PITCH).."degrees ("..getOrientation(ACTUAL_PITCH)..")")
+
+
+local function overlay()
+    YSpeed.setText(round(meta.motionY).."b/s")
+    XSpeed.setText(round(meta.motionX).."b/s")
+    ZSpeed.setText(round(meta.motionZ).."b/s")
+    ThrustSpeed.setText((round(ACTUAL_THRUST)*100).."%")
+    PitchSpeed.setText(round(ACTUAL_PITCH).."degrees ("..getOrientation(ACTUAL_PITCH)..")")
+end
+
 
 local function untilKill(func, doesYield)
     while not stop do
@@ -400,25 +303,19 @@ local function untilKill(func, doesYield)
 end
 
 -- MAIN LOOP
-print("FLY program started, press K to stop")
+print("FLY BALANCER program started, press K to stop")
 
 parallel.waitForAny(
     function() 
         untilKill(refreshMeta, false)
     end,
     function() 
-        untilKill(refreshScan, false)
-    end,
-    function() 
         untilKill(controls, false)
     end,
     function() 
+        untilKill(overlay, true)
+    end,
+    function() 
         untilKill(flyMode, false)
-    end--,
-    --function() 
-    --    untilKill(hoverMode)
-    --end,
-    --function() 
-    --    untilKill(fallCushion)
-    --end
+    end
 )
